a05b996164fe3b9dc7d296acb9fa41ce4f5a0545
ojoan <nwedwols@gmail.com>
2026-06-07 10:46:25 -0300

    aa

---
+++ ex01.cpp
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main() {
+    int v1, v2;
+
+    printf("Enter two values:\n");
+    printf("value 1: ");
+    scanf("%d", &v1);
+    printf("value 2: ");
+    scanf("%d", &v2);
+
+    printf("The value 1 is %d and the value 2 is %d \n", v1, v2);
+}
