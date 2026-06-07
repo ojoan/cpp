9976382693c5a8c348c608608c97b0bc55cfd898
ojoan <nwedwols@gmail.com>
2026-06-07 11:27:15 -0300

    aa

---
+++ ex02.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    float billTotal, divide;
+    int tip, numberPeople;
+
+    printf("Enter the bill total, the tip, and the number of people:\n");
+
+
+    printf("Enter the bill total: ");
+    scanf("%f", &billTotal);
+    printf("Enter the tip: ");
+    scanf("%d", &tip);
+    printf("Enter the number of people: ");
+    scanf("%d", &numberPeople);
+
+    billTotal = billTotal + (billTotal * tip/100);
+    divide = billTotal/numberPeople;
+
+    printf("The amount of the bill: $%.2f\n ", billTotal);
+    printf("The amount divided by %d people was: $%.2f", numberPeople, divide);
+}
