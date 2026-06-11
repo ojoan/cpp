f1a9927bd4b262e2547d718e57f4bcb2cb454856
ojoan <nwedwols@gmail.com>
2026-06-10 23:26:08 -0300

    aa

--- relational operators.cpp
+++ relational operators.cpp
@@ -12,6 +12,14 @@ int main() {
     printf("10 != 10 --> %d\n", 10 != 10);
 
 
+    //check if the number is negative
+    if (-5<0) {
+        printf("negative value");
+    }else {
+        printf("positive value");
+    }
+
+
 
     return 0;
 }
